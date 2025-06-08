70c12d1224b60a0a650aa36bb4d7ae012c20d033
Nicolas <nicolas.olivosm@usm.cl>
2025-06-08 01:13:10 -0400

    Hot-fix issue

--- frontend/src/components/dashboard/overview/latest-orders.tsx
+++ frontend/src/components/dashboard/overview/latest-orders.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -59,7 +61,14 @@ export function LatestOrders({ orders = [], sx }: LatestOrdersProps): React.JSX.
                   <TableCell>{order.customer.name}</TableCell>
                   <TableCell>{dayjs(order.createdAt).format('MMM D, YYYY')}</TableCell>
                   <TableCell>
-                    <Chip color={color} label={label} size="small" />
+                    <Chip 
+                      color={color}
+                      label={label}
+                      size="small" 
+                      onClick={() => {
+                        /* Funcion vacia por ahora para evitar issue*/
+                      }}
+                      />
                   </TableCell>
                 </TableRow>
               );

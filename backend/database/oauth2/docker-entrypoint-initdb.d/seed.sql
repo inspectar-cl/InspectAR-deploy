cc2641dd2bad38b8a99b9b245a0fb9c7e1699ceb
ElVeexo <vicente25852@gmail.com>
2025-10-07 21:27:52 -0300

    Cambios menores de comprensión en rutas, y introducción de user-type:Admin de usuario base para pruebas

--- backend/database/oauth2/docker-entrypoint-initdb.d/seed.sql
+++ backend/database/oauth2/docker-entrypoint-initdb.d/seed.sql
@@ -1,6 +1,9 @@
 -- Insertar users de prueba con IDs específicos
 INSERT INTO users (username, email, password, scope) 
-VALUES ('admin', 'admin@example.com', '$2a$10$AVOlu9c1j6204IpKPjaqcelWG.E5VXPk2538Eu.0j/chBV1ZWBBFG', 'user-type:Analista');
+VALUES ('admin', 'admin@example.com', '$2a$10$AVOlu9c1j6204IpKPjaqcelWG.E5VXPk2538Eu.0j/chBV1ZWBBFG', 'user-type:Admin');
+
+INSERT INTO users (username, email, password, scope) 
+VALUES ('analista', 'analista@example.com', '$2a$10$AVOlu9c1j6204IpKPjaqcelWG.E5VXPk2538Eu.0j/chBV1ZWBBFG', 'user-type:Analista');
 
 INSERT INTO users (username, email, password, scope) 
 VALUES ('tecnico', 'tecnico@example.com', '$2a$10$AVOlu9c1j6204IpKPjaqcelWG.E5VXPk2538Eu.0j/chBV1ZWBBFG', 'user-type:Tecnico');

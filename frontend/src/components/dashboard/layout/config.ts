23d79001109ddc6a129eaff302d74e4b84db013b
Nicolas <nicolas.olivosm@usm.cl>
2025-11-06 04:03:17 -0300

    fix paths

--- frontend/src/components/dashboard/layout/config.ts
+++ frontend/src/components/dashboard/layout/config.ts
@@ -13,7 +13,7 @@ export const navItems = [
   { key: 'graficos', title: 'Gráficos', href: paths.dashboard.graficos, icon: 'chart-line' },
   { key: 'reportes-tecnicos', title: 'Reportes técnicos', href: paths.dashboard.reportesTecnicos, icon: 'file-text' },
   { key: 'reporte-fallas', title: 'Reporte de fallas', href: paths.dashboard.reporteFallas, icon: 'siren' },
-  { key: 'planos', title: 'Planos', href: paths.dashboard.planos, icon: 'blueprint' },
+  //{ key: 'planos', title: 'Planos', href: paths.dashboard.planos, icon: 'blueprint' },
   { key: 'contactos', title: 'Contactos', href: paths.dashboard.contactos, icon: 'adressBook' },
   { key: 'settings', title: 'Settings', href: paths.dashboard.settings, icon: 'gear-six' },
   { key: 'account', title: 'Account', href: paths.dashboard.account, icon: 'user' },
